9875a25b18d1067db925edae6b480360b829680b
Reuben Tier <otterlord.dev@gmail.com>
2025-01-03 18:09:21 +0000

    Implement client-side routing

--- packages/starlight-theme-starpress/src/index.ts
+++ packages/starlight-theme-starpress/src/index.ts
@@ -1,4 +1,12 @@
-import type { StarlightPlugin } from "@astrojs/starlight/types";
+import type { StarlightPlugin, StarlightUserConfig } from "@astrojs/starlight/types";
+
+type ComponentName = keyof NonNullable<StarlightUserConfig['components']>
+
+function createComponents(components: ComponentName[]) {
+	const obj: StarlightUserConfig['components'] = {}
+	components.map(c => {obj[c] = `starlight-theme-starpress/components/${c}.astro`})
+	return obj
+}
 
 export default function createPlugin(): StarlightPlugin {
 	return {
@@ -6,6 +14,9 @@ export default function createPlugin(): StarlightPlugin {
 		hooks: {
 			setup: ({ config, updateConfig }) => {
 				updateConfig({
+					components: createComponents([
+						'Head'
+					]),
 					customCss: [
 						"starlight-theme-starpress/styles/theme.css",
 						...(config.customCss ?? []),
